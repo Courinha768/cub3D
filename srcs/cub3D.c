000cb8350c88dc8717cbcc9b7b6f89ada69e3d65
courinha <antoniocourinha768@gmail.com>
2023-05-09 18:17:39 +0100

    changed everything into minimap

--- srcs/cub3D.c
+++ srcs/cub3D.c
@@ -1,13 +1,5 @@
 #include "../includes/cub3D.h"
 
-int	exit_game(t_data *data)
-{
-	free_map(data->map.map);
-	mlx_destroy_window(data->mm_mlx.ptr, data->mm_mlx.win);
-	exit (1);
-	return (0);
-}
-
 static void	init_vars(char *mapPath, t_data *data)
 {
 	init_map(&data->map, define_map(mapPath));
@@ -21,7 +13,5 @@ void	start_game(char *mapPath, int draw_mm)
 	init_vars(mapPath, &data);
 	if (draw_mm)
 		create_minimap(&data);
-	//mlx_hook(data.mm_mlx.win, ON_KEYDOWN, 0, key_press_hook, &data);
-	mlx_key_hook(data.mm_mlx.win, key_hook, &data);
-	mlx_loop(data.mm_mlx.ptr);
+	else	{}
 }
--- srcs/minimap_utils/create_minimap.c
+++ srcs/minimap_utils/create_minimap.c
@@ -1,10 +1,21 @@
 #include "../../includes/cub3D.h"
 
+int	exit_game(t_data *data)
+{
+	free_map(data->map.map);
+	mlx_destroy_window(data->mm_mlx.ptr, data->mm_mlx.win);
+	exit (1);
+	return (0);
+}
+
 void	create_minimap(t_data *data)
 {
 	init_mm_mlxvars(&data->mm_mlx, data->map.width, data->map.height);
 	data->player.img = create_squareimg(data->mm_mlx.ptr, RGB_RED, 0.5);
 	mlx_hook(data->mm_mlx.win, ON_DESTROY, 0, exit_game, data);
 	mlx_loop_hook(data->mm_mlx.ptr, loop_hook, data);
+	mlx_key_hook(data->mm_mlx.win, key_hook, data);
+	
 	draw_minimap(data);
+	mlx_loop(data->mm_mlx.ptr);
 }
