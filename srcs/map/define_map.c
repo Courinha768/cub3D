efeb2a406e3c87bbc1f4228c414d0a00692a80a3
Antonio maria Appleton monteverde courinha martins <aappleto@c2r4s4.42lisboa.com>
2023-09-01 15:47:59 +0100

    empty lines fix

--- srcs/map/define_map.c
+++ srcs/map/define_map.c
@@ -6,24 +6,29 @@
 /*   By: aappleto <aappleto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/31 15:11:15 by aappleto          #+#    #+#             */
-/*   Updated: 2023/08/31 17:46:06 by aappleto         ###   ########.fr       */
+/*   Updated: 2023/09/01 15:46:53 by aappleto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3D.h"
 
-static char	**get_map(int fd, int lc, char **map)
+static char	**get_map(int fd, int lc, char **map, bool *correct)
 {
-	char	*line;
+	char		*line;
+	static bool	found_map;
 
 	line = get_next_line(fd);
 	while (line && line[0] != ' ' && line[0] != 49)
 	{
+		if (found_map)
+			*correct = false;
 		free(line);
 		line = get_next_line(fd);
 	}
+	if (!found_map)
+		found_map = true;
 	if (line)
-		map = get_map(fd, (lc + 1), map);
+		map = get_map(fd, (lc + 1), map, correct);
 	else if (!map)
 		map = malloc(sizeof(char *) * (lc + 1));
 	if (!map)
@@ -37,9 +42,13 @@ char	**define_map(char *file_path)
 {
 	char	**map;
 	int		fd;
+	bool	correct;
 
+	correct = true;
 	fd = open(file_path, O_RDONLY);
-	map = get_map(fd, 0, NULL);
+	map = get_map(fd, 0, NULL, &correct);
 	close(fd);
+	if (!correct)
+		return (NULL);
 	return (map);
 }
